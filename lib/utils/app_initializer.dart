b70b35457417600f7bae08cd824417185bf98164
Rendani Sinyage <sinyage@gmail.com>
2026-02-11 10:25:24 +0200

    Update app_initializer.dart

--- lib/utils/app_initializer.dart
+++ lib/utils/app_initializer.dart
@@ -56,28 +56,15 @@ class AppInitializer extends StatefulWidget {
             bool? getBool(String key) => config[key] == 1 || config[key] == true || config[key] == "true";
             int? getInt(String key) => int.tryParse(config[key]?.toString() ?? "");
 
-            // Update AppConstants with values from Remote Config
-            if (getString('drawingBaseUrl') != null) AppConstants.drawingBaseUrl = getString('drawingBaseUrl')!;
-            if (getString('adminPageUrl') != null) AppConstants.adminPageUrl = getString('adminPageUrl')!;
-            if (getString('routingKey') != null) AppConstants.routingKey = getString('routingKey')!;
-            if (getString('googleApiKey') != null) AppConstants.googleApiKey = getString('googleApiKey')!;
-            if (getString('firebaseWebKey') != null) AppConstants.firebaseWebKey = getString('firebaseWebKey')!;
-            if (getString('uriPrefix') != null) AppConstants.uriPrefix = getString('uriPrefix')!;
+            // Update AppConstants with behavioral values from Remote Config
             if (getString('isOpen') != null) AppConstants.isOpen = getString('isOpen')!;
+            if (getString('adminPageUrl') != null) AppConstants.adminPageUrl = getString('adminPageUrl')!;
             if (getString('isClosed') != null) AppConstants.isClosed = getString('isClosed')!;
             if (getBool('showGooglePOILayer') != null) AppConstants.showGooglePOILayer = getBool('showGooglePOILayer')!;
             if (getString('localeCodeEn') != null) AppConstants.localeCodeEn = getString('localeCodeEn')!;
-
-            if (getString('demoLatitude') != null) AppConstants.demoLatitude = double.tryParse(getString('demoLatitude')!) ?? 0.0;
-            if (getString('demoLongitude') != null) AppConstants.demoLongitude = double.tryParse(getString('demoLongitude')!) ?? 0.0;
-            if (getString('pinLoadingMin') != null) AppConstants.pinLoadingMin = double.tryParse(getString('pinLoadingMin')!) ?? 0.0;
-            if (getString('pinLoadingMax') != null) AppConstants.pinLoadingMax = double.tryParse(getString('pinLoadingMax')!) ?? 0.0;
-
             if (getInt('newShopDays') != null) AppConstants.newShopDays = getInt('newShopDays')!;
             if (getBool('cardDirect') != null) AppConstants.cardDirect = getBool('cardDirect')!;
             if (getBool('isNumberLengthAlwaysSame') != null) AppConstants.isNumberLengthAlwaysSame = getBool('isNumberLengthAlwaysSame')!;
-            if (getString('countryCodeISO') != null) AppConstants.countryCodeISO = getString('countryCodeISO')!;
-            if (getBool('showFlag') != null) AppConstants.showFlag = getBool('showFlag')!;
             if (getBool('showFlag') != null) AppConstants.showFlag = getBool('showFlag')!;
             if (getBool('showArrowIcon') != null) AppConstants.showArrowIcon = getBool('showArrowIcon')!;
 
